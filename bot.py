05c5eab33777ba8f69e7ee735bff229aa130a171
LostXOR <lostxor@gmail.com>
2023-08-17 16:19:28 -0500

    cleaned up code and changed config file to py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import nextcord, json, os, time, heapq
 from datetime import datetime
 from nextcord.ext import commands
+import config
 
 # Get data for a guild, initializing if it doesn't exist
 def getGuild(guildID):
@@ -17,7 +18,7 @@ def getUser(guildID, userID):
         guildData["users"][userID] = {"xp": 0, "lastxp": 0}
     return guildData["users"][userID]
 
-# Calculate total XP given a level
+# Calculate total XP to reach a level using a very ugly expression
 def calcTotalXP(level):
     return int((10 * level ** 3 + 135 * level ** 2 + 455 * level) / 6)
 
@@ -25,7 +26,7 @@ def calcTotalXP(level):
 def calcXP(level):
     return 5 * level ** 2 + 50 * level + 100
 
-# Calculate XP progress, XP, and level given total XP
+# Calculate XP progress of the current level, total XP for the current level, and the level
 def calcLevel(xp):
     level = 0
     while xp >= calcTotalXP(level + 1):
@@ -33,20 +34,20 @@ def calcLevel(xp):
     return xp - calcTotalXP(level), calcXP(level), level
 
 # Save the database to disk
-def saveDB():
+async def saveDB():
     with open("db.json", "w") as file:
         file.write(json.dumps(db))
 
+# Function to create an embed with parameters
 def createEmbed(name, icon, title, description, author, color):
     embed = nextcord.Embed(title = title, description = description, color = color, timestamp = datetime.now())
     embed.set_author(name = name, icon_url = icon)
     embed.set_footer(text = "Requested by " + author.name, icon_url = author.display_avatar.url)
     return embed
 
-config = json.loads(open("config.json", "r").read())
+# Load JSON database from disk and initialize the bot
 db = json.loads(open("db.json", "r").read()) if os.path.exists("db.json") else {}
 bot = commands.Bot(intents = nextcord.Intents.all())
-
 
 @bot.event
 async def on_ready():
@@ -55,14 +56,15 @@ async def on_ready():
 
 @bot.event
 async def on_message(msg):
+    # Don't keep track of bot XP
     if msg.author.bot:
         return
 
     # Add XP to message sender if they haven't sent a message in xpTimeout seconds
     userData = getUser(msg.guild.id, msg.author.id)
-    if userData["lastxp"] + config["xpTimeout"] < msg.created_at.timestamp():
+    if userData["lastxp"] + config.xpTimeout < msg.created_at.timestamp():
         userData["lastxp"] = msg.created_at.timestamp()
-        userData["xp"] += config["xpPerMessage"]
+        userData["xp"] += config.xpPerMessage
         saveDB()
 
 
@@ -71,48 +73,35 @@ async def level(
     interaction: nextcord.Interaction,
     user: nextcord.Member = nextcord.SlashOption(name = "user", required = False)
 ):
-    if not user:
+    if user is None:
         user = interaction.user
     xpProgress, xpLevel, level = calcLevel(getUser(interaction.guild.id, user.id)["xp"])
     embed = createEmbed(
-        user.name,
-        user.display_avatar.url,
-        f"Level {level}, {xpProgress}/{xpLevel} XP",
-        "",
-        interaction.user, 0x00FF00
-    )
+        user.name, user.display_avatar.url,
+        f"Level {level}, {xpProgress}/{xpLevel} XP", "",
+        interaction.user, 0x00FF00)
     await interaction.send(embeds = [embed])
-
 
 @bot.slash_command(description = "Set a user's level and XP")
 async def set_level(
     interaction: nextcord.Interaction,
     user: nextcord.Member = nextcord.SlashOption(name = "user"),
     level: int = nextcord.SlashOption(name = "level"),
-    xp: int = nextcord.SlashOption(name = "xp")
-):
+    xp: int = nextcord.SlashOption(name = "xp")):
     if interaction.channel.permissions_for(interaction.user).administrator:
         userData = getUser(interaction.guild.id, user.id)
         level = max(min(level, 1000), 0)
         xp = max(min(xp, calcXP(level) - 1), 0)
         userData["xp"] = calcTotalXP(level) + xp
         embed = createEmbed(
-            user.name,
-            user.display_avatar.url,
-            f"Set to level {level}, {xp}/{calcXP(level)} XP",
-            "",
-            interaction.user,
-            0x00FF00
-        )
+            user.name, user.display_avatar.url,
+            f"Set to level {level}, {xp}/{calcXP(level)} XP", "",
+            interaction.user, 0x00FF00)
     else:
         embed = createEmbed(
-            "",
-            None,
-            "Only administrators can use this command!",
-            "",
-            interaction.user,
-            0xFF0000
-        )
+            "", None,
+            "Only administrators can use this command!", "",
+            interaction.user, 0xFF0000)
     await interaction.send(embeds = [embed])
     saveDB()
 
@@ -123,9 +112,17 @@ async def leaderboard(
     page: int = nextcord.SlashOption(name = "page", required = False)
 ):
     users = getGuild(interaction.guild.id)["users"]
-    page = max(min(page, (len(users) - 1) // config["pageSize"] + 1), 1) if page else 1
-    leaderboard = heapq.nlargest(page * config["pageSize"], users.items(), key = lambda u: u[1]["xp"])[(page - 1) * config["pageSize"]:]
 
+    # Calculate maximum pages and clamp page number
+    if page is None:
+        page = 1
+    maxPages = -(-len(users) // config.pageSize)
+    page = max(min(page, maxPages), 1)
+
+    # Get the highest players up to the right page and chop off the unneeded pages before
+    leaderboard = heapq.nlargest(page * config.pageSize, users.items(), key = lambda u: u[1]["xp"])[(page - 1) * config.pageSize:]
+
+    # Generate leaderboard text
     leaderboardText = ""
     for i in range(len(leaderboard)):
         xpProgress, xpLevel, level = calcLevel(leaderboard[i][1]["xp"])
@@ -134,15 +131,14 @@ async def leaderboard(
             name = user.name
         except:
             name = "Unknown User"
-        leaderboardText += f"{i + (page - 1) * config['pageSize'] + 1}. {name}, Level {level}, {xpProgress}/{xpLevel} XP\n"
+        leaderboardText += f"{i + (page - 1) * config.pageSize + 1}. {name}, Level {level}, {xpProgress}/{xpLevel} XP\n"
+
+    # Send embed
     embed = createEmbed(
         interaction.guild.name,
         interaction.guild.icon.url if interaction.guild.icon else None,
-        f"Leaderboard, page {page}/{(len(users) - 1) // config['pageSize'] + 1}",
-        leaderboardText,
-        interaction.user,
-        0x00FF00
-    )
+        f"Leaderboard, page {page}/{(len(users) - 1) // config.pageSize + 1}",
+        leaderboardText, interaction.user, 0x00FF00)
     await interaction.send(embeds = [embed])
 
-bot.run(config["botToken"])
+bot.run(config.botToken)
---
+++ config.py
@@ -0,0 +1,4 @@
+botToken = "<YOUR TOKEN HERE>"
+xpPerMessage = 20
+xpTimeout = 60
+pageSize = 10

6f48869d9044480ae8ce676039060353d53ac6ca
LostXOR <lostxor@gmail.com>
2023-10-23 14:02:23 -0500

    Added hardcoded role selector

--- bot.py
+++ bot.py
@@ -11,6 +11,35 @@ async def on_ready():
     await bot.change_presence(activity = status)
     print("Bot started")
 
+# Hardcoded role buttons
+@bot.event
+async def on_interaction(intr):
+    rolesMessage = 1010024261659918396
+    robotButton = "reaction_roles.button.1010024258791018496"
+    humanButton = "reaction_roles.button.1010024258790891520"
+    robotRole = 1005309355182260327
+    humanRole = 831569900777635922
+    # Wrong message
+    if intr.message.id != rolesMessage:
+        return
+    # Figure out what roles need to be added and removed
+    if intr.data["custom_id"] == robotButton:
+        roleToRemove = robotRole if intr.user.get_role(robotRole) else humanRole if intr.user.get_role(humanRole) else None
+        roleToAdd = robotRole if not intr.user.get_role(robotRole) else None
+    elif intr.data["custom_id"] == humanButton:
+        roleToRemove = humanRole if intr.user.get_role(humanRole) else robotRole if intr.user.get_role(robotRole) else None
+        roleToAdd = humanRole if not intr.user.get_role(humanRole) else None
+    # Add/remove roles
+    if roleToAdd: await intr.user.add_roles(nextcord.Object(roleToAdd))
+    if roleToRemove: await intr.user.remove_roles(nextcord.Object(roleToRemove))
+    # Send response to interaction
+    if roleToAdd and not roleToRemove: embedText = f"Added <@&{roleToAdd}>"
+    elif roleToRemove and not roleToAdd: embedText = f"Removed <@&{roleToRemove}>"
+    else: embedText = f"Added <@&{roleToAdd}> and removed <@&{roleToRemove}>"
+    await intr.send(embeds = [
+        embed.createEmbed(intr.user, "", embedText, intr.user, 0x00FF00)
+    ], ephemeral = True)
+
 @bot.event
 async def on_message(msg):
     # Respond to mistypings of !rank (Added at the request of TinRobit)
